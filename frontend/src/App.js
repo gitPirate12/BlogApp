3adc9883fc51e0c7ef4ac9714d28897bc4c3675f
gitPirate12 <aneeqshaffy7@gmail.com>
2024-12-20 16:45:31 +0530

    react static homepage done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,76 @@
-
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <main>
       <header>
-        <a href= " " className= "logo" >MyBlog</a>
+        <a href=" " className="logo">
+          MyBlog
+        </a>
         <nav>
-          <a href= " " >Login</a>
-          <a href= " " >Register</a>
+          <a href=" ">Login</a>
+          <a href=" ">Register</a>
         </nav>
       </header>
-      <div className= "entries">
-        <div className="entry"></div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/10/GettyImages-2169504261-e1729802377786.jpg?w=1024"
+            alt="random"
+          />
+        </div>
+        <div className="texts">
+          <h2>Perplexity has reportedly closed a $500M funding round</h2>
+          <p className="info">
+            <a className="author">Aneeq Shaffy</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            AI-powered search engine Perplexity has reportedly closed a $500
+            million funding round, valuing the startup at $9 billion. Bloomberg,
+            citing sources familiar, reports that the round was led by
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/10/GettyImages-2169504261-e1729802377786.jpg?w=1024"
+            alt="random"
+          />
+        </div>
+        <div className="texts">
+          <h2>Perplexity has reportedly closed a $500M funding round</h2>
+          <p className="info">
+            <a className="author">Aneeq Shaffy</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            AI-powered search engine Perplexity has reportedly closed a $500
+            million funding round, valuing the startup at $9 billion. Bloomberg,
+            citing sources familiar, reports that the round was led by
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2024/10/GettyImages-2169504261-e1729802377786.jpg?w=1024"
+            alt="random"
+          />
+        </div>
+        <div className="texts">
+          <h2>Perplexity has reportedly closed a $500M funding round</h2>
+          <p className="info">
+            <a className="author">Aneeq Shaffy</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            AI-powered search engine Perplexity has reportedly closed a $500
+            million funding round, valuing the startup at $9 billion. Bloomberg,
+            citing sources familiar, reports that the round was led by
+          </p>
+        </div>
       </div>
     </main>
   );
