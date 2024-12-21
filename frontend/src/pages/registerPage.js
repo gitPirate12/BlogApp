bcc4059454a9a09f6b98002c93d108a05f867802
gitPirate12 <aneeqshaffy7@gmail.com>
2024-12-21 15:43:10 +0530

    started workign on backend

--- frontend/src/pages/registerPage.js
+++ frontend/src/pages/registerPage.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import registerPage from "./registerPage";
 
 function registerPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
   return (
-    <form className='register'>
-        <h1>Register</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button>Register</button>
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button>Register</button>
     </form>
-  )
+  );
 }
 
-export default registerPage
+export default registerPage;
